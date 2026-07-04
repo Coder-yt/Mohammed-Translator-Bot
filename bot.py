8f3900c394180b0eef58950d7083042abbc9e53a
MohammedDev-yt <animeworldday2005@gmail.com>
2026-07-05 00:38:33 +0530

    bot.py

---
+++ bot.py
@@ -0,0 +1,44 @@
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+import handlers.start
+import handlers.translate
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+app = Client(
+    "TranslationBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+if __name__ == "__main__":
+    print("=" * 50)
+    print("🌍 Translation Bot Started Successfully")
+    print("=" * 50)
+
+    app.run()
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
