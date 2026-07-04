73484e4e83033cbf33908619ea6b6a9cdb9c152d
MohammedDev-yt <animeworldday2005@gmail.com>
2026-07-05 00:50:28 +0530

    keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,25 @@
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host="0.0.0.0", port=10000)
+
+def keep_alive():
+    t = threading.Thread(target=run)
+    t.start()
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
